6970d634dcba40f69337c37bc45366021b6d0684
Martin <abuinmartin@hotmail.com>
2019-03-20 21:42:45 -0300

    Subo la clase con el If

---
+++ java-inicial/src/ar/edu/utn/java/inicial/condicionante/CondicionanteIf.java
@@ -0,0 +1,24 @@
+package ar.edu.utn.java.inicial.condicionante;
+
+public class CondicionanteIf {
+	
+    public static void main(String[] args) {
+		
+    	
+    	int valor = 0;
+    	
+    	
+    	if(valor < 0) {
+    		// bloque de codigo si se cumple
+    		// la condicion
+    		System.out.println("Valor es negativo");
+    		
+    	}  else if (valor>0)
+    	    { System.out.println("Valor es positivo");
+    	    }
+    	else {
+    		System.out.println("Valor es cero");
+                }
+    	
+}
+}
