49ad0bf42f47186363137e8d14ad50800e4dfd49
Martin <abuinmartin@hotmail.com>
2019-03-29 01:24:00 -0300

    Mi segundo commit

---
+++ java-inicial/src/ar/edu/utn/condicionantes/switchcase/HolaMundo.java
@@ -0,0 +1,28 @@
+package ar.edu.utn.condicionantes.switchcase;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		
+
+	
+		
+		//for(int i=1;i<=22;i++) { Creciente
+		  
+		   for(int i=22;i>1;i--) {
+		
+			/*
+			 * Operador trinario:
+			 * condicion ? accion por verdadero : accion por falso
+			 * 
+			 * Operador modulo %
+			 * el resultado de la operacion modulo
+			 * devuelve el valor del resto de la division
+			 * por ej.: 5 % 2 resultado es 1
+			 * por ej.: 16 % 2 resultado es 0
+			 */
+			   
+		   }
+	}
+}
+
