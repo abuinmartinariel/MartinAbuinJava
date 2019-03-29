28fd22106eaba954e6a856c0878746aff1b87e47
Martin <mlennard@gmail.com>
2019-03-29 19:42:53 -0300

    Mi cuarto commit

--- java-inicial/src/ar/edu/utn/condicionantes/switchcase/HolaMundo.java
+++ java-inicial/src/ar/edu/utn/condicionantes/switchcase/HolaMundo.java
@@ -10,11 +10,6 @@ public class HolaMundo {
 		//for(int i=1;i<=22;i++) { Creciente
 		  
 		   for(int i=22;i>1;i--) {
-			   
-			   if(i%2==0) {
-				   
-				   System.out.println("Hola Mundo " + i + " veces");
-			   }
 		
 			/*
 			 * Operador trinario:
@@ -26,8 +21,21 @@ public class HolaMundo {
 			 * por ej.: 5 % 2 resultado es 1
 			 * por ej.: 16 % 2 resultado es 0
 			 */
-			   
-		   }
-	}
-}
+			
+			
+			
+			
+			//int solo_pares = i%2==0?i:-1;
+		
+		
+			if(i%2==0) {
+			
+			System.out.println("Hola Mundo! " + i + ((i>1)?" veces":" vez"));
+			
+			
+	
 
+        }
+   }
+ }
+}
