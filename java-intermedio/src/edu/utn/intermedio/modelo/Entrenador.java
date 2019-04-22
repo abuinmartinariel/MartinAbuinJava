7ca99b1bb24db0ba43d79a423eb456260a2a0aef
unknown <sebastianieva2000@gmail.com>
2019-04-22 19:52:22 -0300

    JAVA INTERMEDIO

--- java-intermedio/src/edu/utn/intermedio/modelo/Entrenador.java
+++ java-intermedio/src/edu/utn/intermedio/modelo/Entrenador.java
@@ -1,59 +1,54 @@
 package edu.utn.intermedio.modelo;
 
+
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class Entrenador {
+public class Entrenador extends Persona {
 	
 	private int cantidadMaximaBoxeadores;
-	private String nombre;
 	private Collection<Boxeador> boxeadores;
 	private Categoria[] categorias;
-	
-	
-	public Entrenador(String y, Categoria cat1, Categoria cat2, int maximo) {
-		
-		nombre = y;
+
+	public Entrenador(String y, Categoria cat1, 
+			Categoria cat2, int maximo) {
+		this.setNombre(y);
 		categorias = new Categoria[2];
 		categorias[0] = cat1;
 		categorias[1] = cat2;
 		boxeadores = new ArrayList<Boxeador>();
 		cantidadMaximaBoxeadores = maximo;
-		
-		
 	}
-		public void entrenar() {
-			
-			
-			
-			
-		}
-		
-			
-		public boolean asignarBoxeador(Boxeador b) {
-			
-			if(boxeadores.size() >= cantidadMaximaBoxeadores ) {
+	
+	public void entrenar() {
+	}
+	
+	public boolean asignarBoxeador(Boxeador b) {
+		if(boxeadores.size() >= cantidadMaximaBoxeadores) {
 			return false;
-			
-			}
-			else {
-				boxeadores.add(b);
-				
-				
-			}
-			
-			
-			return true;
+		} else {
+			boxeadores.add(b);
 		}
-		
-		public void imprimirBoxeadores() {
-			for (Boxeador boxeador : boxeadores) {
-				
-				System.out.println(boxeador);
-				
-			}
-		
+		return true;
 	}
+	
+	public void imprimirBoxeadores() {
+		for (Boxeador boxeador : boxeadores) {
+			System.out.println(boxeador);
+		}
+	}
+
+	@Override
+	public void presentarse() {
+		StringBuilder presentacion = new StringBuilder();
+		presentacion.append(getIdentificacion());
+		presentacion.append(" : ");
+		presentacion.append("[").append(this.categorias[0]);
+		presentacion.append(",").append(this.categorias[1]);
+		presentacion.append("]");
+		System.out.println(presentacion.toString());
+	}
+	
+	
+
 }
-
-
