7e456ee63ce50d71c348e3e588aabf26e9c0d69c
Martin <abuinmartin@hotmail.com>
2019-04-29 21:41:03 -0300

    JAVA INTERMEDIO

---
+++ java-intermedio/src/edu/utn/intermedio/main/TestLambda.java
@@ -0,0 +1,55 @@
+package edu.utn.intermedio.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+public class TestLambda {
+
+	public static void main(String[] args) {
+        List<Integer> enteros = new ArrayList<>();
+        
+        for(int i = 1; i <= 20; i++){
+        	
+        	enteros.add(i);
+           }
+        
+        
+        
+        System.out.println("Los valores son: ");
+
+        enteros.forEach(ent -> System.out.println(ent));
+        
+        /* enteros.replaceAll(ent -> {
+        	            if(ent % 2 == 1) {
+        	            double potencia = Math.pow(ent, 2);
+        	            Double cuadrado = Double.valueOf(potencia);
+        	            return cuadrado.intValue();
+        	            }
+        	            
+        
+                     return ent;
+                      });
+        
+        
+        System.out.println("Los valores al cuadrado son: ");
+        enteros.forEach(ent -> System.out.println(ent));
+        */
+       
+        //Elimino los multiplos de 4
+        
+        enteros.removeIf(ent -> esMultiploDeCuatro(ent));
+      
+        
+        System.out.println("Sin los multiplos de 4: ");
+        enteros.forEach(i -> System.out.print(i+" "));
+            
+	}
+	
+	
+	public static boolean esMultiploDeCuatro(Integer entero) {
+		return entero % 4 == 0;
+		
+	}
+
+}
